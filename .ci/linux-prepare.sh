chaudron/ovs

commit 234e626198a4d99b6839061ca7e123f5c31c5ebe

ci: Use previous sarif-tools release due to issue in latest release.
The just released v3.0 of the sarif tools do not work as
expected when comparing results. Temporarily force pip
to install the 2.0 release until this is fixed.

Acked-by: Aaron Conole <aconole@redhat.com>
Acked-by: Simon Horman <horms@ovn.org>
Signed-off-by: Eelco Chaudron <echaudro@redhat.com>

.ci/linux-prepare.sh
@@ -23,7 +23,7 @@ cd ..
 #     https://github.com/pypa/pip/issues/10655
 pip3 install --disable-pip-version-check --user wheel
 pip3 install --disable-pip-version-check --user \
-    flake8 netaddr pyparsing sarif-tools sphinx setuptools
+    flake8 netaddr pyparsing sarif-tools==2.0.0 sphinx setuptools
 
 # Install python test dependencies
 pip3 install -r python/test_requirements.txt

commit 7cebb9b5496959f3a0e00429719274d8d0a28c6e

ci: Use sarif-tools v3.0.1 due to issues in earlier versions.
Sarif-tools v3.0 introduced an issue that has been resolved in v3.0.1.
Ensure that v3.0.1 or higher is installed via pip.

Fixes: 234e626198a4 ("ci: Use previous sarif-tools release due to issue in latest release.")
Signed-off-by: Eelco Chaudron <echaudro@redhat.com>

.ci/linux-prepare.sh
@@ -23,7 +23,7 @@ cd ..
 #     https://github.com/pypa/pip/issues/10655
 pip3 install --disable-pip-version-check --user wheel
 pip3 install --disable-pip-version-check --user \
-    flake8 netaddr pyparsing sarif-tools==2.0.0 sphinx setuptools
+    flake8 netaddr pyparsing sarif-tools>=3.0.1 sphinx setuptools
 
 # Install python test dependencies
 pip3 install -r python/test_requirements.txt