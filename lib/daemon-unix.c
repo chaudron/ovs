3f33e673f61642123cd21392d84860a0a529403a
Eelco Chaudron <echaudro@redhat.com>
2025-06-04 17:43:34 +0200

    daemon-unix: Handle potential negative values from sysconf().
    Coverity reports that daemon_set_new_user() may receive a large
    unsigned value from get_sysconf_buffer_size(), due to sysconf()
    returning -1 and being cast to size_t.
    
    Although this would likely lead to an allocation failure and abort,
    it's better to handle the error in place.
    
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>

--- lib/daemon-unix.c
+++ lib/daemon-unix.c
@@ -915,8 +915,9 @@ daemon_become_new_user(bool access_datapath, bool access_hardware_ports)
 static size_t
 get_sysconf_buffer_size(void)
 {
-    size_t bufsize, pwd_bs = 0, grp_bs = 0;
     const size_t default_bufsize = 1024;
+    long pwd_bs = 0, grp_bs = 0;
+    size_t bufsize;
 
     errno = 0;
     if ((pwd_bs = sysconf(_SC_GETPW_R_SIZE_MAX)) == -1) {
